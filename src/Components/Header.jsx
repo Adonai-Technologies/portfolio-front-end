5a311f3845c9a3e982890b0f38af9a07e37612d1
Akua Konadu <akuakkonadu8@gmail.com>
2024-04-23 16:15:50 +0100

    modify

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Header() {
 	return (
 		<div className='p-5 bg-primary flex justify-between header'>
-			<h1 className='text-secondary text-4xl font-semibold'>ADO-Tech</h1>
-			<h1 className='text-white text-4xl font-semibold'>A</h1>
-			<h1 className='text-tertiary text-4xl font-semibold'>K</h1>
+			<h1 className='text-secondary text-4xl font-semibold'>ADO-Tech</h1>;
+			<h1 className='text-white text-4xl font-semibold'>A</h1>;
+			<h1 className='text-tertiary text-4xl font-semibold'>K</h1>;
 		</div>
 	);
 }
